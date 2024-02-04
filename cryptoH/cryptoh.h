d69d985f096e5a8629ba1c6b7ce0a013d55e94e5
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-02-04 13:23:03 +0530

    include <stddef.h> for size_t.

--- cryptoH/cryptoh.h
+++ cryptoH/cryptoh.h
@@ -4,6 +4,8 @@
 #define nullptr (void*)0
 #define hashLogSize 36
 
+#include <stddef.h>
+
 char hashLog[hashLogSize] = { '0', '8', '2', '3', '4', '9', '1', '7', '6', '5', 'A', 'C', 'B', 'P', 'D', 'F', 'I', 'J', 'M', 'E', 'H', 'G', 'Y', 'K', 'Z', 'X', 'T', 'L', 'N', 'O', 'S', 'U', 'R','W','V','Q','\0' };
 
 //Function prototypes
