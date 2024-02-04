4d3447d52833dfef81f7ccb2e13ecc221d3aec6f
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-02-04 13:24:31 +0530

    remove null char in hashLog.

--- cryptoH/cryptoh.h
+++ cryptoH/cryptoh.h
@@ -6,7 +6,7 @@
 
 #include <stddef.h>
 
-char hashLog[hashLogSize] = { '0', '8', '2', '3', '4', '9', '1', '7', '6', '5', 'A', 'C', 'B', 'P', 'D', 'F', 'I', 'J', 'M', 'E', 'H', 'G', 'Y', 'K', 'Z', 'X', 'T', 'L', 'N', 'O', 'S', 'U', 'R','W','V','Q','\0' };
+char hashLog[hashLogSize] = { '0', '8', '2', '3', '4', '9', '1', '7', '6', '5', 'A', 'C', 'B', 'P', 'D', 'F', 'I', 'J', 'M', 'E', 'H', 'G', 'Y', 'K', 'Z', 'X', 'T', 'L', 'N', 'O', 'S', 'U', 'R','W','V','Q'};
 
 //Function prototypes
 char* hash(char* data, char* password, size_t len);
